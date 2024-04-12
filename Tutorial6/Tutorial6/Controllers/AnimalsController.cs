04933f41e6007f764b662366e2fefaf293b51ab4
Andrii Kulyzhskyi <kulyzhskyi.andrii@gmail.com>
2024-04-12 18:43:45 +0200

    Init commit with some additions

---
+++ Tutorial6/Tutorial6/Controllers/AnimalsController.cs
@@ -0,0 +1,48 @@
+﻿using System.IO.Pipelines;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using Tutorial6.Models;
+
+namespace Tutorial6.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class AnimalsController : ControllerBase
+{
+    private readonly IConfiguration _configuration;
+
+    public AnimalsController(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+    
+    [HttpGet]
+    public IActionResult GetAnimals()
+    {
+        
+        using SqlConnection connection = new SqlConnection(_configuration.GetConnectionString("Default"));
+        connection.Open();
+
+        using SqlCommand command = new SqlCommand();
+        command.Connection = connection;
+        command.CommandText = "SELECT * FROM Animal";
+
+        var reader = command.ExecuteReader();
+
+        var animals = new List<Animal>();
+
+        int idAnimalOrdinal = reader.GetOrdinal("IdAnimal");
+        int nameOrdinal = reader.GetOrdinal("Name");
+
+        while (reader.Read())
+        {
+            animals.Add(new Animal()
+            {
+                IdAnimal = reader.GetInt32(idAnimalOrdinal),
+                Name = reader.GetString(nameOrdinal)
+            });
+        }
+        
+        return Ok(animals);
+    }
+}
---
+++ Tutorial6/Tutorial6/Models/Animal.cs
@@ -0,0 +1,7 @@
+﻿namespace Tutorial6.Models;
+
+public class Animal
+{
+    public int IdAnimal { get; set; }
+    public string Name { get; set; }
+}
